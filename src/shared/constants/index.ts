15da6a9c4a33535f9c2a95e64f36c7f449a1fcbf
hyungyunlim <hyungyun.lim@gmail.com>
2026-04-18 12:36:01 +0900

    chore: release 3.3.0

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,7 +2,7 @@
  * AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
  *
  * Source: shared/constants/index.ts
- * Generated: 2026-04-18T00:24:38.558Z
+ * Generated: 2026-04-18T03:35:08.829Z
  *
  * To modify, edit the source file in shared/constants/ and run:
  *   npm run sync:shared
--- src/shared/platforms/definitions.ts
+++ src/shared/platforms/definitions.ts
@@ -2,7 +2,7 @@
  * AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
  *
  * Source: shared/platforms/definitions.ts
- * Generated: 2026-04-18T00:24:38.554Z
+ * Generated: 2026-04-18T03:35:08.828Z
  *
  * To modify, edit the source file in shared/platforms/ and run:
  *   npm run sync:shared
