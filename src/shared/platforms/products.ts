8bd140cbfb736fa24a928e6dc3a06b6defe608dc
hyungyunlim <hyungyun.lim@gmail.com>
2026-08-13 21:36:52 +0900

    chore: release 4.6.9

--- src/shared/platforms/products.ts
+++ src/shared/platforms/products.ts
@@ -2,7 +2,7 @@
  * AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
  *
  * Source: shared/platforms/products.ts
- * Generated: 2026-08-06T05:11:27.900Z
+ * Generated: 2026-08-13T12:36:35.273Z
  *
  * To modify, edit the source file in shared/platforms/ and run:
  *   npm run sync:shared
