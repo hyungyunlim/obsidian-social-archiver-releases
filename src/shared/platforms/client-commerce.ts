7a8f98cceb9824cc678e0e41c391d82a9d87d63e
hyungyunlim <hyungyun.lim@gmail.com>
2026-08-16 07:49:56 +0900

    chore: release 4.7.0

--- src/shared/platforms/client-commerce.ts
+++ src/shared/platforms/client-commerce.ts
@@ -2,7 +2,7 @@
  * AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
  *
  * Source: shared/platforms/client-commerce.ts
- * Generated: 2026-08-02T11:32:26.264Z
+ * Generated: 2026-08-15T22:49:39.191Z
  *
  * To modify, edit the source file in shared/platforms/ and run:
  *   npm run sync:shared
