a9cd86d148d7fa5801fa84cec695056c9308d033
hyungyunlim <hyungyun.lim@gmail.com>
2026-09-02 09:32:30 +0900

    chore: release 4.7.3

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,7 +2,7 @@
  * AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
  *
  * Source: shared/constants/index.ts
- * Generated: 2026-07-07T04:01:02.553Z
+ * Generated: 2026-09-02T00:32:16.926Z
  *
  * To modify, edit the source file in shared/constants/ and run:
  *   npm run sync:shared
