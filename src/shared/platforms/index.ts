f8c8036daa03aabead29995b2a46cc32d5d575a3
hyungyunlim <hyungyun.lim@gmail.com>
2026-09-20 07:01:53 +0900

    chore: release 4.7.8

--- src/shared/platforms/index.ts
+++ src/shared/platforms/index.ts
@@ -2,7 +2,7 @@
  * AUTO-GENERATED FILE - DO NOT EDIT DIRECTLY
  *
  * Source: shared/platforms/index.ts
- * Generated: 2026-09-15T01:53:47.619Z
+ * Generated: 2026-09-19T22:01:39.805Z
  *
  * To modify, edit the source file in shared/platforms/ and run:
  *   npm run sync:shared
